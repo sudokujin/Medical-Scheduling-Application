2afc709bb9d47ac0694769b608ba8d9baaecf0d6
Qing Jin <qingjinfullstack@gmail.com>
2023-04-15 13:42:03 -0500

    omg

--- java/src/main/java/com/techelevator/dao/JdbcDoctorTimeDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcDoctorTimeDao.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+@Component
 
 @Component
 public class JdbcDoctorTimeDao implements DoctorTimeDao{
